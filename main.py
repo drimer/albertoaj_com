2ddadf5b92b29595ba221c0137eb558aa41c8b93
Alberto Aguilera <drimer.aaj@gmail.com>
2013-10-11 14:05:40 +0100

    static: move all CSS/JS files and images to "static" folder
    Now it will be easier for web servers (ie. apache, nginx) to deploy the
    files under static and cache them instead of having flask deploy them on
    every request.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from werkzeug.routing import BaseConverter
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.', static_path='/static')
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
 
 
 @app.route("/")
 def hello():
-    return "Site under construction"
+    return render_template('index.html')
+
+
+@app.route('/<regex(".*"):file_basename>.html')
+def contact(file_basename):
+    return render_template('%s.html' % file_basename)
 
 
 if __name__ == "__main__":
