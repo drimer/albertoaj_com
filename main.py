e269309b3096d2c6b58e5e108a0946e7a266a4b6
Alberto Aguilera <alberto.aguilera@youview.com>
2013-10-25 18:53:17 +0100

    main.py: Refactor code that sets up the regular expressions
    This way, it's more clear what that code does, and if it needs to be
    moved around, it will be centralized in a unit, the function
    `add_regex_support()`.

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from werkzeug.routing import BaseConverter
 
 from library import template
 
+
+def add_regex_support():
+    class RegexConverter(BaseConverter):
+        def __init__(self, url_map, *items):
+            super(RegexConverter, self).__init__(url_map)
+            self.regex = items[0]
+
+    app.url_map.converters['regex'] = RegexConverter
+
+
 app = Flask(__name__, template_folder='templates', static_path='/static')
-
-
-class RegexConverter(BaseConverter):
-    def __init__(self, url_map, *items):
-        super(RegexConverter, self).__init__(url_map)
-        self.regex = items[0]
-
-
-app.url_map.converters['regex'] = RegexConverter
-
-
+add_regex_support()
 
 
 @app.route("/")
